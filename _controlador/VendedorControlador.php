49b235610a34e56da9e50d697cf2c0609fc5638a
sebastianxjavier@gmail.com <sebastianxjavier@gmail.com@5e72fe3e-217f-f1ea-b111-f1a8156abb4e>
2012-06-08 22:25:08 +0000

    Completado el modulo de cobros y reporte por cliente

--- _controlador/VendedorControlador.php
+++ _controlador/VendedorControlador.php
@@ -11,14 +11,122 @@
  
  function cobrar()
  {
+	 require("_modelo/Cliente.php");
+	 require("_modelo/Usuario.php");
+	 require("_modelo/Documento_Pago.php");
+	 
+	 if (isset($_REQUEST['txtNombre']))
+	 {
+		 $codigo_factura = $_REQUEST['txtNombre'];
+	 
+	 	 $vendedor = new Vendedor();
+	 	 $documentox = $vendedor->realizarCobro($codigo_factura);
+		
+		 $num_rows = mysql_num_rows($documentox);
+		
+		 if($num_rows != 0)
+		 {
+			 while ($registro=mysql_fetch_assoc($documentox))
+			 {
+				 if($registro['v_existe'] == 2)
+				 {
+					 $documento = new Documento_Pago();
+					 $documento->setIdDocumentoPago($registro['id_documento_pago']);
+					 $documento->setFechaEmisionDocumentoPago($registro['fecha_emision_documento_pago']);
+					 $documento->setFechaVencimientoDocumentoPago($registro['fecha_vencimiento_documento_pago']);
+					 $documento->setTotalDocumentoPago($registro['total_documento_pago']);
+					 $documento->setEstadoDocumentoPago($registro['estado_documento_pago']);
+					 $cliente = new Cliente();
+					 $cliente->setIdCliente($registro['cliente_id_cliente']);
+					 break;
+				 }
+				 elseif($registro['v_existe'] == 1)
+				 {
+					 echo("La factura N '$codigo_factura' se encuentra pagada o anulada.");
+					 exit;
+				 }
+				 else
+				 {
+					 echo("La factura N '$codigo_factura' no existe en el sistema.");
+					 exit;
+				 }				 
+			 }
+			 require("_vista/actualizarEstadoCobro.php");
+		 }
+		 else
+			 echo("La factura N° '$codigo_factura' no existe en el sistema.");
+	 }
+	 elseif(isset($_REQUEST['txtCodigo']) && isset($_REQUEST['cboEstado']))
+	 {
+		 $documento = new Documento_Pago();
+		 $documento->setIdDocumentoPago($_REQUEST['txtCodigo']);
+		 $documento->setEstadoDocumentoPago($_REQUEST['cboEstado']);
+		 
+		 $vendedor = new Vendedor();
+		 $documentoxx = $vendedor->cambiarEstadoCobro($documento->getIdDocumentoPago(),$documento->getEstadoDocumentoPago());
+		 
+		 while ($registro = mysql_fetch_array($documentoxx))
+		 {
+			 $existe = $registro['v_existe'];
+			 break;
+		 }
+		 
+		 if ($existe == 1)
+			 echo "La Factura '".$documento->getIdDocumentoPago()."' ha sido ".strtolower($documento->getEstadoDocumentoPago())." 
+			 satisfactoriamente.";
+		 else
+		 	 echo "La Factura '".$documento->getIdDocumentoPago()."' no existe en el sistema.";
+	 }	 
+	 else
+	 	 require("_vista/realizarCobro.php");		 		 
  }
  
  function listarclientes()
  {
+	 if (isset($_REQUEST['txtNombre']))
+	 {
+		 require("_modelo/Cliente.php");
+		 require("_modelo/Usuario.php");
+		 require("_modelo/Documento_Pago.php");
+		 
+		 $nombre_cliente = $_REQUEST['txtNombre'];
+		 
+		 $vendedor = new Vendedor();
+		 $documentox = $vendedor->listarFactura($nombre_cliente);
+	 
+		 $num_rows = mysql_num_rows($documentox);
+	 
+		 if($num_rows != 0)
+	 	 {
+			 /*while ($registro = mysql_fetch_assoc($documentox))
+			 {
+				 $documento = new Documento_Pago();
+				 $documento->setIdDocumentoPago($registro['id_documento_pago']);
+				 $documento->setFechaEmisionDocumentoPago($registro['fecha_emision_documento_pago']);
+				 $documento->setFechaVencimientoDocumentoPago($registro['fecha_vencimiento_documento_pago']);
+				 $documento->setTotalDocumentoPago($registro['total_documento_pago']);
+				 $documento->setEstadoDocumentoPago($registro['estado_documento_pago']);
+			 }*/
+			 require("_vista/ventasxCliente2.php");
+		 }
+		 else
+		 	 echo "El cliente '$nombre_cliente' no existe en el sistema";
+			 exit;
+	 }
+	 else
+		 require("_vista/ventasxCliente.php");
  }
  
  function listarclientesmorosos()
  {
+	 if (isset($_REQUEST['txtNombre']))
+	 {
+		 echo "gua";
+		 /*require("_vista/ventasxCliente2.php");*/
+	 }
+	 else
+	 	 echo "listar clientes morosos";
+		 /*require("_vista/ventasxCliente.php");*/
  }
  
  function alertascobros()
