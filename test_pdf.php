f1987be670f673ec9ea3e523effed5a0c40b836e
arturoperez@live.cl <arturoperez@live.cl@5e72fe3e-217f-f1ea-b111-f1a8156abb4e>
2012-06-18 01:20:47 +0000

    incluye creacion de imagen

--- test_pdf.php
+++ test_pdf.php
@@ -1,6 +1,7 @@
 <?php
 	require('lib/fpdf/fpdf.php');
 	$pdf = new FPDF();
+	
 	//Obtenemos los datos que identifican a la factura
 	$numfactura = strtoupper($_REQUEST['txtFactura']);
 	$dia = strtoupper($_REQUEST['txtDia']);
@@ -10,24 +11,27 @@
 	$direccion = strtoupper($_REQUEST['txtDireccion']);
 	$rut = strtoupper($_REQUEST['txtRut']);
 	$giro = strtoupper($_REQUEST['txtGiro']);
-	//$ciudad = strtoupper($_REQUEST['cboCiudad']);
-	//$comuna = strtoupper($_REQUEST['cboComuna']);
+	$ciudad = strtoupper($_REQUEST['txtCiudad']);
+	$comuna = strtoupper($_REQUEST['txtComuna']);
 	$fono = strtoupper($_REQUEST['txtTelefono']);
 	$orden = strtoupper($_REQUEST['txtOrden']);
 	$guia = strtoupper($_REQUEST['txtGuia']);
 	$condiciones = strtoupper($_REQUEST['txtCondiciones']);
 	$vencimiento = strtoupper($_REQUEST['txtVencimiento']);
+	
 	//Obtenemos el contenido de la factura en arrays
 	$cantidad = $_POST['txtCantidad'];
 	$detalle = $_POST['txtDetalle'];
 	$preunitario = $_POST['txtUnitario'];
 	$descuento = $_POST['txtDescuento'];
 	$total = $_POST['txtTotal'];
+	
 	//Obtenemos el neto, iva y total de la factura
 	$neto = strtoupper($_REQUEST['txtNeto']);
 	$iva = strtoupper($_REQUEST['txtIva']);
 	$total2 = strtoupper($_REQUEST['txtTotal2']);
 	$pdf->AddPage();
+	
 	//Numero de Factura
 	$pdf->SetFont('Arial','B',14);
 	$pdf->Cell(160);
@@ -45,7 +49,7 @@
 	$pdf->Cell(20,10,"Dirección: $direccion",0,0,'L');
 	//Comuna
 	$pdf->Cell(80);
-	$pdf->Cell(20,10,"Comuna: Cerrillos",0,0,'L');
+	$pdf->Cell(20,10,"Comuna: $comuna",0,0,'L');
 	$pdf->Ln();
 	//Rut
 	$pdf->Cell(20,10,"RUT: $rut",0,0,'L');
@@ -96,5 +100,41 @@
 	$pdf->Cell(45,7,$total2,1,0,'C');
 	$pdf->Ln();
 	//Guardamos la factura
-	$pdf->Output('facturas_pdf/' . $numfactura.'.pdf', 'F'); 
+	$pdf->Output('facturas_pdf/' . $numfactura.'.pdf', 'F');
+	
+	/*_______________________________________________________*/
+	// 				CREACION IMAGEN JPG						 //
+	/*_______________________________________________________*/
+	
+	$image = imagecreatefromjpeg('notaventa.jpg');
+	$background = imagecolorallocate($image,0,0,0);
+	$color= imagecolorallocate($image,0,0,0);
+	$fuente = 'arial.ttf';
+	
+	imagettftext($image, 15,0,679,61,$color,$fuente, $numfactura);
+	imagettftext($image, 10,0,166,189,$color,$fuente, $cliente);
+	imagettftext($image, 10,0,544,189,$color,$fuente, $rut);
+	//imagettftext($image, 15,0,208,192,$color,$fuente, $atte);
+	$fecha = "$dia de $mes del $año";
+	imagettftext($image, 10,0,555,205,$color,$fuente, $fono);
+	imagettftext($image, 10,0,106,220,$color,$fuente, $fecha);
+	imagettftext($image, 10,0,568,220,$color,$fuente, $ciudad);
+	//imagettftext($image, 10,0,585,235,$color,$fuente, $vendedor);
+	
+	$lugar = 330;
+	for($i=0;$i<count($detalle);$i++){
+		imagettftext($image, 10,0,140,$lugar,$color,$fuente, $cantidad[$i]);
+		imagettftext($image, 10,0,220,$lugar,$color,$fuente, $detalle[$i]);
+		imagettftext($image, 10,0,570,$lugar,$color,$fuente, $preunitario[$i]);
+		imagettftext($image, 10,0,646,$lugar,$color,$fuente, $total[$i]);
+		$lugar = $lugar + 21;
+	}
+	
+	imagettftext($image, 10,0,646,813,$color,$fuente, $neto);
+	imagettftext($image, 10,0,646,834,$color,$fuente, $iva);
+	imagettftext($image, 10,0,646,855,$color,$fuente, $total2);
+	
+	header('Location: index.php');
+	imagejpeg($image,'facturas_imagen/' . $numfactura.'.jpg');
+	
 ?>
