39840ef748a695d0aee17671610b325a86670f33
sebastianxjavier@gmail.com <sebastianxjavier@gmail.com@5e72fe3e-217f-f1ea-b111-f1a8156abb4e>
2012-06-18 03:26:30 +0000

    Modificación del archivo "acercade.php"

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Acerca de SVCB</title>
+</head>
+
+<body>
+<h1>Acerca de SVCB</h1>
+<p>Copyright © 2012 Informática Arte Bismarck.</p>
+<p>Arte Bismarck es una marca registrada.</p>
+<p>Tecnologías usadas en el desarrollo de este sistema:</p>
+<table width="100" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><img src="lib/img/logos/html5.png" width="110" height="110" /></td>
+    <td><img src="lib/img/logos/css3.jpg" width="110" height="55" /></td>
+    <td><img src="lib/img/logos/php.png" width="110" height="58" /></td>
+  </tr>
+  <tr>
+    <td><img src="lib/img/logos/mysql.png" width="110" height="57" /></td>
+    <td><img src="lib/img/logos/jquery.png" width="110" height="27" /></td>
+    <td><img src="lib/img/logos/ajax.jpg" width="110" height="73" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
