391b63977b1f9fd2d41ab14ba8326b27898eb51d
sebastianxjavier@gmail.com <sebastianxjavier@gmail.com@5e72fe3e-217f-f1ea-b111-f1a8156abb4e>
2012-06-22 15:32:54 +0000

    Actualizacion de las query

--- lib/ajax/cliente_factura.php
+++ lib/ajax/cliente_factura.php
@@ -1,23 +1,17 @@
-<?php  
-	require_once("conexion.php");
-	$search_term = mysql_real_escape_string($_POST['id']);
-	$query = mysql_query("SELECT * FROM cliente WHERE nombre_cliente = '$search_term'");
-	$row = mysql_fetch_object($query);
-	$jsondata['id_cliente'] = $row->id_cliente;
-	$jsondata['nombre_cliente'] = $row->nombre_cliente;
-	$jsondata['direccion_cliente'] = $row->direccion_cliente;
-	$jsondata['telefono_cliente'] = $row->telefono_cliente;
-	$jsondata['mail_cliente'] = $row->mail_cliente;
-	$jsondata['giro_cliente'] = $row->giro_cliente;
-	
-	$comuna = $row->comuna_id_comuna;
-	$query2 = mysql_query("SELECT * FROM comuna WHERE id_comuna = '$comuna'");
-	$row2 = mysql_fetch_object($query2);
-	$jsondata['nombre_comuna'] = $row2->nombre_comuna;
-	
-	$provincia = $row2->provincia_id_provincia;
-	$query3 = mysql_query("SELECT * FROM provincia WHERE id_provincia = '$provincia'");
-	$row3 = mysql_fetch_object($query3);
-	$jsondata['nombre_provincia'] = $row3->nombre_provincia;
-	echo json_encode($jsondata); 
+<?php
+  require_once("conexion.php");
+  $search_term = mysql_real_escape_string($_POST['id']);
+  $sql = "SELECT cl.id_cliente, cl.direccion_cliente, cl.telefono_cliente, cl.mail_cliente, cl.giro_cliente, co.nombre_comuna, ";
+  $sql.= "pr.nombre_provincia FROM cliente cl, comuna co, provincia pr WHERE cl.comuna_id_comuna = co.id_comuna AND ";
+  $sql.= "co.provincia_id_provincia = pr.id_provincia AND nombre_cliente = '$search_term'";
+  $query = mysql_query($sql);
+  $row = mysql_fetch_object($query);
+  $jsondata['id_cliente'] = $row->id_cliente;
+  $jsondata['direccion_cliente'] = $row->direccion_cliente;
+  $jsondata['telefono_cliente'] = $row->telefono_cliente;
+  $jsondata['mail_cliente'] = $row->mail_cliente;
+  $jsondata['giro_cliente'] = $row->giro_cliente;
+  $jsondata['nombre_comuna'] = $row->nombre_comuna;
+  $jsondata['nombre_provincia'] = $row->nombre_provincia;
+  echo json_encode($jsondata);
 ?>
--- lib/ajax/producto_factura.php
+++ lib/ajax/producto_factura.php
@@ -4,15 +4,5 @@
 	$query = mysql_query("SELECT * FROM producto WHERE nombre_producto = '$search_term'");
 	$row = mysql_fetch_object($query);
 	$jsondata['precio_producto'] = $row->precio_producto;
-	//$jsondata['total_producto'] = $row->total_producto;	
-	
-	/*$comuna = $row->comuna_id_comuna;
-	$query2 = mysql_query("SELECT * FROM comuna WHERE id_comuna = '$comuna'");
-	$row2 = mysql_fetch_object($query2);
-	$jsondata['comuna_id_comuna'] = $row2->nombre_comuna;
-	$provincia = $row2->provincia_id_provincia;
-	$query3 = mysql_query("SELECT * FROM provincia WHERE id_provincia = '$provincia'");
-	$row3 = mysql_fetch_object($query3);
-	$jsondata['nombre_comuna'] = $row3->nombre_provincia;*/
 	echo json_encode($jsondata); 
 ?>
