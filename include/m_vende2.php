59fe6464dd3375d55d787c7c777bbafbd05eb896
arturoperez@live.cl <arturoperez@live.cl@5e72fe3e-217f-f1ea-b111-f1a8156abb4e>
2012-08-20 20:43:25 +0000

    menu nuevo de vendedor

---
+++ include/m_vende2.php
@@ -0,0 +1,36 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<ul class="top-level">
+	<li><a href="vendedor">Inicio</a></li>
+    <li>
+    	<a href="#">Gestión de Clientes</a>
+        <ul class="sub-level">
+        	<li><a href="indexx.php?controlador=cliente&accion=ingresar">Ingresar Cliente</a></li>
+            <li><a href="indexx.php?controlador=cliente&accion=listar">Listar Cliente</a></li>
+            <li><a href="indexx.php?controlador=cliente&accion=modificar">Modificar Cliente</a></li>
+            <li><a href="indexx.php?controlador=cliente&accion=eliminar">Eliminar Cliente</a></li>
+        </ul>
+    </li>
+    <li>
+    	<a href="#">Ventas</a>
+        <ul class="sub-level">
+        	<li><a href="indexx.php?controlador=vendedor&accion=vender">Realizar Venta</a></li>   
+            <li><a href="indexx.php?controlador=vendedor&accion=cobrar">Realizar Cobro</a></li>
+			<li><a href="indexx.php?controlador=vendedor&accion=listarnotasdeventa">Notas de Venta</a></li>
+        </ul>
+	</li>
+    <li>
+    	<a href="#">Reportes</a>
+        <ul class="sub-level">
+        	<li><a href="indexx.php?controlador=vendedor&accion=listarclientes">Ventas por Cliente</a></li>   
+            <li><a href="indexx.php?controlador=vendedor&accion=listarclientesmorosos">Clientes Morosos</a></li>
+        </ul>
+	</li>
+    <li>
+    	<a href="#">Ayuda</a>
+        <ul class="sub-level">
+        	<li><a href="#">Video Tutorial</a></li>
+            <li><a href="#">Manual de Usuario</a></li>
+            <li><a href="about.php">Acerca de SVCB</a></li>
+        </ul>
+    </li>
+</ul>
